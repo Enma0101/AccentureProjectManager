63b9d5a3eb937aac01df74663eb2c2c422f88f01
Enma0101 <enmanuel.aft@gmail.com>
2025-07-01 12:55:53 -0400

    cambio final

--- frontend/src/pages/administrator/Help.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import HelpCenter from "../../components/common/HelpCenter";
-import "../../stylesheets/help.css";
-import "../../stylesheets/topcontrols.css";
-
-export default function Help() {
-  return <HelpCenter role="admin" />;
-}
